db5cb5461d2fd31894943ab9946377fd3c34febb
robertlek <lecarobertandrei@gmail.com>
2024-04-05 23:27:17 +0300

    Implemented the first API controller that will handle the quiz type CRUD operations.

---
+++ QuizMastery.Business/Models/Response.cs
@@ -0,0 +1,11 @@
+﻿using System.Net;
+
+namespace QuizMastery.Business.Models;
+
+public class Response
+{
+    public HttpStatusCode StatusCode { get; set; }
+    public bool IsSuccess { get; set; } = true;
+    public List<string> ErrorMessages { get; set; } = [];
+    public object Result { get; set; } = new object();
+}
---
+++ QuizMastery.Web/Controllers/QuizTypeController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QuizMastery.Business.Models;
+using QuizMastery.Business.Services.QuizTypeService;
+using QuizMastery.DataAccess.Entities;
+using System.Net;
+
+namespace quiz_mastery_api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class QuizTypeController(IQuizTypeService quizTypeService) : ControllerBase
+{
+    private readonly IQuizTypeService _quizTypeService = quizTypeService;
+    private readonly Response _response = new();
+
+    [HttpGet]
+    [Route("GetAllQuizTypes")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<Response>> GetAllQuizTypes()
+    {
+        try
+        {
+            IEnumerable<QuizType> quizTypes = await _quizTypeService.GetAllAsync();
+
+            _response.Result = quizTypes;
+            _response.StatusCode = HttpStatusCode.OK;
+
+            return Ok(_response);
+        }
+        catch (Exception exception)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessages.Add(exception.Message);
+        }
+
+        return _response;
+    }
+}
