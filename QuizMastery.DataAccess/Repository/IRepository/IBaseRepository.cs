f1ec9a3d0643c30b20a7267ec142e8943ea770c7
robertlek <lecarobertandrei@gmail.com>
2024-04-06 16:10:50 +0300

    Repository 'Add' and 'Update' methods can now return the target entity.

--- QuizMastery.DataAccess/Repository/IRepository/IBaseRepository.cs
+++ QuizMastery.DataAccess/Repository/IRepository/IBaseRepository.cs
@@ -4,10 +4,10 @@ namespace QuizMastery.DataAccess.Repository.IRepository;
 
 public interface IBaseRepository<T> where T : class
 {
-    Task AddAsync(T entity);
+    Task<T> AddAsync(T entity);
     Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
     Task<T?> GetAsync(Expression<Func<T, bool>> filter, bool tracked = true);
     Task RemoveAsync(T entity);
     Task SaveAsync();
-    Task UpdateAsync(T entity);
+    Task<T> UpdateAsync(T entity);
 }
